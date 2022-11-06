6dc839833d9b8d86a4f849565de956c629f7b814
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-11-06 20:30:02 +0200

    Create a base door class

---
+++ Source/TrickyDoors/Private/DoorBase.cpp
@@ -0,0 +1,159 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "DoorBase.h"
+
+#include "TimelineAnimationComponent.h"
+
+
+ADoorBase::ADoorBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	DoorAnimationComponent = CreateDefaultSubobject<UTimelineAnimationComponent>("DoorAnimation");
+}
+
+void ADoorBase::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	switch (InitialState)
+	{
+	case EDoorState::Opened:
+		DoorAnimationComponent->InitialState = ETimelineAnimationState::End;
+		break;
+
+	case EDoorState::Closed:
+		DoorAnimationComponent->InitialState = ETimelineAnimationState::Begin;
+		break;
+
+	default:
+		break;
+	}
+}
+
+void ADoorBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CurrentState = InitialState;
+}
+
+void ADoorBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ADoorBase::Open()
+{
+	if (CurrentState != EDoorState::Closed)
+	{
+		return;
+	}
+
+	DoorAnimationComponent->Start();
+}
+
+void ADoorBase::Close()
+{
+	if (CurrentState != EDoorState::Opened)
+	{
+		return;
+	}
+
+	DoorAnimationComponent->Start();
+}
+
+void ADoorBase::SetIsLocked(const bool bIsLocked)
+{
+	if (CurrentState == EDoorState::Transition || CurrentState == EDoorState::Disabled)
+	{
+		return;
+	}
+
+	if (bIsLocked)
+	{
+		if (CurrentState == EDoorState::Locked)
+		{
+			return;
+		}
+
+		PreviousState = CurrentState;
+		CurrentState = EDoorState::Locked;
+	}
+	else
+	{
+		if (CurrentState != EDoorState::Locked)
+		{
+			return;
+		}
+
+		CurrentState = PreviousState;
+		PreviousState = EDoorState::Locked;
+	}
+
+	OnStateChanged.Broadcast(CurrentState);
+}
+
+void ADoorBase::SetIsEnabled(const bool bIsEnabled)
+{
+	if (bIsEnabled)
+	{
+		if (CurrentState != EDoorState::Disabled)
+		{
+			return;
+		}
+
+		if (PreviousState == EDoorState::Transition)
+		{
+			DoorAnimationComponent->Resume();
+		}
+
+		CurrentState = PreviousState;
+		PreviousState = EDoorState::Disabled;
+		SetActorTickEnabled(true);
+	}
+	else
+	{
+		if (CurrentState == EDoorState::Disabled)
+		{
+			return;
+		}
+
+		if (CurrentState == EDoorState::Transition)
+		{
+			DoorAnimationComponent->Pause();
+		}
+
+		PreviousState = CurrentState;
+		CurrentState = EDoorState::Disabled;
+		SetActorTickEnabled(false);
+	}
+
+	OnStateChanged.Broadcast(CurrentState);
+}
+
+void ADoorBase::ChangeState(const ETimelineAnimationState NewAnimationState)
+{
+	PreviousState = CurrentState;
+
+	switch (NewAnimationState)
+	{
+	case ETimelineAnimationState::Begin:
+		CurrentState = EDoorState::Closed;
+		break;
+
+	case ETimelineAnimationState::End:
+		CurrentState = EDoorState::Opened;
+		break;
+
+	case ETimelineAnimationState::Transition:
+		CurrentState = EDoorState::Transition;
+		break;
+		
+	default:
+		break;
+	}
+	
+	OnStateChanged.Broadcast(CurrentState);
+}
---
+++ Source/TrickyDoors/Public/DoorBase.h
@@ -0,0 +1,70 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TimelineAnimationComponent.h"
+#include "GameFramework/Actor.h"
+#include "TimelineAnimationComponent.h"
+#include "DoorBase.generated.h"
+
+
+UENUM()
+enum class EDoorState : uint8
+{
+	Closed,
+	Opened,
+	Transition,
+	Locked,
+	Disabled
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnStateChangedSignature, EDoorState, NewState);
+
+UCLASS()
+class TRICKYDOORS_API ADoorBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ADoorBase();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintAssignable, Category="Door")
+	FOnStateChangedSignature OnStateChanged;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Door")
+	EDoorState InitialState = EDoorState::Closed;
+
+	UFUNCTION(BlueprintCallable, Category="Door")
+	void Open();
+
+	UFUNCTION(BlueprintCallable, Category="Door")
+	void Close();
+
+	UFUNCTION(BlueprintCallable, Category="Door")
+	void SetIsLocked(const bool bIsLocked);
+
+	UFUNCTION(BlueprintCallable, Category="Door")
+	void SetIsEnabled(const bool bIsEnabled);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	UTimelineAnimationComponent* DoorAnimationComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Door")
+	EDoorState CurrentState = EDoorState::Closed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Door")
+	EDoorState PreviousState = EDoorState::Closed;
+
+	UFUNCTION()
+	void ChangeState(const ETimelineAnimationState NewAnimationState);
+};
