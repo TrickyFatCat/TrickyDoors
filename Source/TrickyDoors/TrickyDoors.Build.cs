914251971921b6f2b381d095e1aa07b04049c6ba
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-11-06 20:29:35 +0200

    Add dependencies

--- Source/TrickyDoors/TrickyDoors.Build.cs
+++ Source/TrickyDoors/TrickyDoors.Build.cs
@@ -38,6 +38,8 @@ public class TrickyDoors : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"TrickyAnimationComponents",
+				"TrickyInteractionSystem",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
