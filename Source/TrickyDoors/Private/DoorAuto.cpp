d77130bccb17e84fd13d7bb966d95ddf77c7df41
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-11-07 00:00:25 +0200

    Create an automatic door class

---
+++ Source/TrickyDoors/Private/DoorAuto.cpp
@@ -0,0 +1,73 @@
+// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "DoorAuto.h"
+
+#include "KeyringLibrary.h"
+#include "KeyType.h"
+#include "Components/BoxComponent.h"
+
+ADoorAuto::ADoorAuto()
+{
+	bIsReversible = true;
+
+	ActivationTriggerComponent = CreateDefaultSubobject<UBoxComponent>("ActivationTrigger");
+	ActivationTriggerComponent->SetupAttachment(GetRootComponent());
+	ActivationTriggerComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	ActivationTriggerComponent->SetCollisionObjectType(ECC_WorldDynamic);
+	ActivationTriggerComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	ActivationTriggerComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void ADoorAuto::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ActivationTriggerComponent->OnComponentBeginOverlap.AddDynamic(this, &ADoorAuto::OnActivationTriggerBeginOverlap);
+	ActivationTriggerComponent->OnComponentEndOverlap.AddDynamic(this, &ADoorAuto::OnActivationTriggerEndOverlap);
+}
+
+void ADoorAuto::OnActivationTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+                                                AActor* OtherActor,
+                                                UPrimitiveComponent* OtherComp,
+                                                int32 OtherBodyIndex,
+                                                bool bFromSweep,
+                                                const FHitResult& SweepResult)
+{
+	if (!IsValid(OtherActor))
+	{
+		return;
+	}
+	
+	if (bIsClosingDelayed)
+	{
+		StopAutoClosingTimer();
+	}
+
+	if (KeyClass && bRequiredKey)
+	{
+		if (UKeyringLibrary::ActorUseKey(OtherActor, KeyClass))
+		{
+			Open();
+		}
+	}
+	else
+	{
+		Open();
+	}
+}
+
+void ADoorAuto::OnActivationTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent,
+                                              AActor* OtherActor,
+                                              UPrimitiveComponent* OtherComp,
+                                              int32 OtherBodyIndex)
+{
+	if (bIsClosingDelayed)
+	{
+		StartAutoClosingTimer(ClosingDelayDuration);
+	}
+	else
+	{
+		Close();
+	}
+}
---
+++ Source/TrickyDoors/Public/DoorAuto.h
@@ -0,0 +1,54 @@
+// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DoorBase.h"
+#include "DoorAuto.generated.h"
+
+class UBoxComponent;
+class UKeyType;
+/**
+ * 
+ */
+UCLASS()
+class TRICKYDOORS_API ADoorAuto : public ADoorBase
+{
+	GENERATED_BODY()
+
+public:
+	ADoorAuto();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Coponents")
+	UBoxComponent* ActivationTriggerComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Door")
+	bool bRequiredKey = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Door", meta=(EditCondition="bRequiredKey"))
+	TSubclassOf<UKeyType> KeyClass = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Door")
+	bool bIsClosingDelayed = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Door", meta=(EditCondition="bAutoClosingEnabled"))
+	float ClosingDelayDuration = 3.f;
+
+private:
+	UFUNCTION()
+	void OnActivationTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+	                                     AActor* OtherActor,
+	                                     UPrimitiveComponent* OtherComp,
+	                                     int32 OtherBodyIndex,
+	                                     bool bFromSweep,
+	                                     const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnActivationTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent,
+	                                   AActor* OtherActor,
+	                                   UPrimitiveComponent* OtherComp,
+	                                   int32 OtherBodyIndex);
+};
